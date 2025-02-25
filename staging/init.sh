1232c5df7bc5fcef3597779efedfecbf755622e9
Martijn Stegeman <martijn@stgm.nl>
2025-02-25 18:28:02 +0100

    prepare db on reload

--- staging/init.sh
+++ staging/init.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+# reset the database to blank
 # bin/rails db:reset
 bin/rails db:prepare
+
+# clear caches
 bin/rails tmp:clear
+
+# start Solid Queue job processor in the background
+bin/jobs &
+
+# start application server
 passenger start
