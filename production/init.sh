96daa0537b3baff1ea4bf3979bbbc09032708c3a
Martijn Stegeman <m.stegeman@uva.nl>
2025-05-09 05:55:26 +0200

    update baseimage, add job runner for production (#14)
    * update baseimage, add job runner for production
    
    * baseimage to latest release

--- production/init.sh
+++ production/init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# update database schema or initialize new database
 bin/rails db:prepare
+
+# start Solid Queue job processor in the background
+bin/jobs &
+
+# start application server
 passenger start
